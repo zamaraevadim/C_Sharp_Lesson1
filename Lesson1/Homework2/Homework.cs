abee2f91fd6e8643d63e74f304cbda65cd018831
Vadim <zamaraevadim@yandex.ru>
2023-07-10 15:57:11 +0500

    Исправил структуру репозитотрия

---
+++ Lesson1/Homework2/Homework.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Введите два числа: ");
+int number1 = Int32.Parse(Console.ReadLine());
+int number2 = Int32.Parse(Console.ReadLine());
+if(number1 > number2){
+    Console.WriteLine(number1);
+}else{
+    Console.WriteLine(number2);
+}
