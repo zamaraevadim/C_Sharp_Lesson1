3862ec90124380b7a93f2943242b3f65eda968d6
Vadim <zamaraevadim@yandex.ru>
2023-07-10 13:03:17 +0500

    Задача 2

--- HelloConsole/Program.cs
+++ HelloConsole/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Введите число: ");
+// int number = Int32.Parse(Console.ReadLine());
+// Console.WriteLine(number*number);
+
+// Console.WriteLine("Введите два числа: ");
+// int num1 = Int32.Parse(Console.ReadLine());
+// int num2 = Int32.Parse(Console.ReadLine());
+
+// if(num2 * num2 == num1){
+//     Console.WriteLine("Да");
+// }else{
+//     Console.WriteLine("Нет");
+// }
+
+// Console.WriteLine("Введите номе дня недели: ");
+// int num1 = Int32.Parse(Console.ReadLine());
+// if(num1 == 1 ){
+//     Console.WriteLine("Понедельник");
+// }else if(num1 == 2){
+//     Console.WriteLine("Вторник");
+// }else if(num1 == 3){
+//     Console.WriteLine("Среда");
+// }else if(num1 == 4){
+//     Console.WriteLine("Четверг");
+// }else if(num1 == 5){
+//     Console.WriteLine("Пятница");
+// }else if(num1 == 6){
+//     Console.WriteLine("Суббота");
+// }else if(num1 == 7){
+//     Console.WriteLine("Воскресенье");
+// }else{
+//     Console.WriteLine("Нет такого дня");
+// }
+
+// Console.WriteLine("Введите число: ");
+// int num = Int32.Parse(Console.ReadLine());
+// if(num < 0){
+//     num = num * -1;
+// }
+// for(int x = -num; x <= num; x++){
+//     Console.Write(x + " ");
+// }
+
