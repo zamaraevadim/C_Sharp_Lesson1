2c256db6e67e93c47f420de86437a7ee1b8cb871
Vadim <zamaraevadim@yandex.ru>
2023-07-18 12:48:01 +0500

    Классная работа

---
+++ Lesson5/Classwork/Program.cs
@@ -0,0 +1,54 @@
+﻿// int[] nums = new int[12];
+// int positive_sum = 0;
+// int negative_sum = 0;
+// for(int i = 0;i < nums.Length;i++){
+//     nums[i] = new Random().Next(-9,10);
+//     Console.Write(nums[i] + " ");
+//     if(nums[i] < 0){
+//         negative_sum += nums[i];
+//     }else{
+//         positive_sum += nums[i];
+//     }
+// }
+// Console.WriteLine("Сумма положительных - " + positive_sum);
+// Console.WriteLine("Сумма отрицательных - " + negative_sum);
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// int[] nums = new int[12];
+
+// for(int i = 0;i < nums.Length;i++){
+//     nums[i] = new Random().Next(-9,10);
+//     Console.Write(nums[i] + " ");
+// }
+// Console.WriteLine();
+// for(int j = 0;j < nums.Length;j++){
+
+//     nums[j] = nums[j] * -1;
+   
+//     Console.Write(nums[j] + " ");
+// }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+int[] nums = new int[12];
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0;i < nums.Length;i++){
+    nums[i] = new Random().Next(-9,10);
+    Console.Write(nums[i] + " ");
+}
+Console.WriteLine();
+string result = "нет";
+for(int j = 0;j < nums.Length;j++){
+    if(nums[j] == num){
+        result = "Да";
+        break;
+    }
+    
+}
+Console.WriteLine(result);
